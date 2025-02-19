5ef45a0ac0d66fe95d308c2b3d805c65d5d2c1c2
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-02-18 20:05:29 -0500

    new index.html file, Jackson's idea

--- personal_website/views.py
+++ personal_website/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request,'about.html')
