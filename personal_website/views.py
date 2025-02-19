227da298adfb9ed50b1d48a290c5379f754b963f
Porter Morgan <portermorgan@vl965-172-31-134-115.wireless.umass.edu>
2025-02-18 19:43:58 -0500

    renamed home.html to index.html

--- personal_website/views.py
+++ personal_website/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def about(request):
     return render(request,'about.html')
